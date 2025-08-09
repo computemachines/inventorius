c88b86a3d7e142eb98bef63b315dfedf161668bb
Tyler Parker <tparker@computemachines.com>
2025-08-09 16:21:54 -0400

    Squashed 'inventorius-api/' content from commit e5ae94e
    git-subtree-dir: inventorius-api
    git-subtree-split: e5ae94e53ad8a18d92190f0e5f2e64bde9ce3d10

---
+++ conftest.py
@@ -0,0 +1,47 @@
+import contextlib
+import sys
+from pathlib import Path
+
+import pytest
+from hypothesis import settings
+
+from flask import g, request_started
+
+# Ensure the application package on the src/ path is importable
+sys.path.insert(0, str(Path(__file__).resolve().parent / "src"))
+from inventorius import app as inventorius_flask_app
+from inventorius.db import get_mongo_client
+
+
+# give tests longer to complete on ci server
+settings.register_profile("ci", deadline=500)
+settings.load_profile("ci")
+
+# from contextlib import contextmanager
+
+
+def subscriber(sender):
+    g.db = get_mongo_client().testing
+
+
+request_started.connect(subscriber, inventorius_flask_app)
+
+
+@pytest.fixture
+def client():
+    inventorius_flask_app.testing = True
+    yield inventorius_flask_app.test_client()
+    # close app
+
+
+@contextlib.contextmanager
+def clientContext():
+    inventorius_flask_app.testing = True
+    inventorius_flask_app.secret_key = "1234"
+    test_db = get_mongo_client().testing
+    test_db.admin.delete_many({})
+    test_db.batch.delete_many({})
+    test_db.bin.delete_many({})
+    test_db.sku.delete_many({})
+    test_db.user.delete_many({})
+    yield inventorius_flask_app.test_client()
